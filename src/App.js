230d207366dc7893bee155f2418a4612f9e239bd
v.lipko <v.lipko@bacup.ru>
2021-12-27 18:13:02 +0700

    feat: fetch movies and display list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import MovieCard from './movie-card/movie-card';
+import { Box, Grid } from '@mui/material';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = 'bebd61ac069805fc5f54de2f0e6b838e';
+class App extends React.Component {
+  cardList = [];
+
+  render() {
+    return (
+      <Box sx={{ padding: '16px 0 0 16px' }}>
+        <Grid container spacing={2}>
+          {this.cardList}
+        </Grid>
+      </Box>
+    );
+  }
+
+  async componentDidMount() {
+    const url = new URL('https://api.themoviedb.org/3/movie/popular');
+    url.searchParams.append('api_key', API_KEY);
+
+    const res = await fetch(url.toString());
+    const payload = await res.json();
+    this.cardList = this.createCardList(payload.results);
+
+    this.setState({ cardList: this.cardList });
+  }
+
+  createCardList(movies) {
+    return movies.map((movie) => (
+      <Grid item key={movie.id}>
+        <MovieCard movie={movie} />
+      </Grid>
+    ));
+  }
 }
 
 export default App;
---
+++ src/movie-card/movie-card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+
+function MovieCard(props) {
+  const ASPECT_RATIO = 4 / 3;
+  const size = 200;
+  const cssProps = { height: Math.round(ASPECT_RATIO * size), width: size };
+  return (
+    <Card sx={cssProps}>
+      <CardActionArea component="a">
+        <CardMedia
+          component="img"
+          height="204"
+          image={`https://image.tmdb.org/t/p/original${props.movie.poster_path}`}
+          alt={props.movie.original_title}
+        />
+        <CardContent sx={{ display: 'flex' }}>
+          <Tooltip title={props.movie.title}>
+            <Typography component="span" variant="h6" noWrap>
+              {props.movie.title}
+            </Typography>
+          </Tooltip>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default MovieCard;
