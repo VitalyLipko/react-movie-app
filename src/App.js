03396be4667149a5864867c49541b9ba88fb0766
v.lipko <v.lipko@bacup.ru>
2021-12-28 11:58:12 +0700

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-import MovieCard from './movie-card/movie-card';
-import { Box, Grid } from '@mui/material';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import Popular from './Popular/Popular';
 
-const API_KEY = 'bebd61ac069805fc5f54de2f0e6b838e';
-class App extends React.Component {
-  cardList = [];
-
-  render() {
-    return (
-      <Box sx={{ padding: '16px 0 0 16px' }}>
-        <Grid container spacing={2}>
-          {this.cardList}
-        </Grid>
-      </Box>
-    );
-  }
-
-  async componentDidMount() {
-    const url = new URL('https://api.themoviedb.org/3/movie/popular');
-    url.searchParams.append('api_key', API_KEY);
-
-    const res = await fetch(url.toString());
-    const payload = await res.json();
-    this.cardList = this.createCardList(payload.results);
-
-    this.setState({ cardList: this.cardList });
-  }
-
-  createCardList(movies) {
-    return movies.map((movie) => (
-      <Grid item key={movie.id}>
-        <MovieCard movie={movie} />
-      </Grid>
-    ));
-  }
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Popular />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Popular/Popular.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Box, Grid } from '@mui/material';
+import MovieCard from '../movie-card/movie-card';
+
+const API_KEY = 'bebd61ac069805fc5f54de2f0e6b838e';
+
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { cardList: [] };
+  }
+
+  async componentDidMount() {
+    const url = new URL('https://api.themoviedb.org/3/movie/popular');
+    url.searchParams.append('api_key', API_KEY);
+
+    const res = await fetch(url.toString());
+    const payload = await res.json();
+
+    this.setState({ cardList: this.createCardList(payload.results) });
+  }
+
+  createCardList(movies) {
+    return movies.map((movie) => (
+      <Grid item key={movie.id.toString()}>
+        <MovieCard movie={movie} />
+      </Grid>
+    ));
+  }
+
+  render() {
+    return (
+      <Box sx={{ padding: '16px 0 0 16px' }}>
+        <Grid container spacing={2}>
+          {this.state.cardList}
+        </Grid>
+      </Box>
+    );
+  }
+}
+
+export default Popular;
